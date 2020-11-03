29f67c2d79adc11c4b846ef8af004b40b592aa1e
Oleksandr Slipchenko <o.p.slipchenko@gmail.com>
2020-11-03 18:11:23 +0200

    Добавляет вариант таймера от ментора

--- src/index.js
+++ src/index.js
@@ -1,18 +1,71 @@
 import './styles.css';
 
+// Вариант от ментора ////////////////////////////// не убирает слова days, hours, minutes, seconds
+
+// class CountdownTimer {
+//   constructor({ selector, targetDate }) {
+//     this.targetDate = new Date(targetDate);
+
+//     this.days = document.querySelector(`${selector} .value[data-value="days"]`);
+//     this.hours = document.querySelector(`${selector} .value[data-value="hours"]`);
+//     this.minutes = document.querySelector(`${selector} .value[data-value="mins"]`);
+//     this.seconds = document.querySelector(`${selector} .value[data-value="secs"]`);
+//   }
+//   init() {
+//     setInterval(() => this._countDown(), 1000)
+//   }
+//   _updateTime(totalSeconds) {
+//     this.days.textContent = Math.floor(totalSeconds / 3600 / 24);
+//     this.hours.textContent =
+//       this._formatTime(Math.floor((totalSeconds / 3600) % 24));
+//     this.minutes.textContent = this._formatTime(Math.floor((totalSeconds / 60) % 60));
+//     this.seconds.textContent = this._formatTime(Math.floor(totalSeconds % 60));
+//   }
+//   _countDown() {
+//     const currentDate = new Date();
+//     const totalSeconds = (this.targetDate - currentDate) / 1000;
+//     this._updateTime(totalSeconds)
+//   }
+//   _formatTime(time) {
+//     return time < 10 ? `0${time}` : time;
+//   }
+// }
+
+
+// const timer = new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: '1 Jan 2021',
+// });
+
+// timer.init();
+
+// Мой вариант //////////////////////////////////////
+
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.targetDate = new Date(targetDate);
 
+    this.title = document.querySelector('body');
     this.days = document.querySelector( `${selector} .value[data-value="days"]`);
     this.hours = document.querySelector( `${selector} .value[data-value="hours"]`);
     this.minutes = document.querySelector( `${selector} .value[data-value="mins"]`);
     this.seconds = document.querySelector(`${selector} .value[data-value="secs"]`);
     this.labels = document.querySelectorAll(`${selector} .label`);
   }
+  newStringMarkUp() {
+    return `<p>Новый Год через:</p>`;
+  }
+
   startPage() {
-    const time = this.getTimeComponents(0);
-    this.updateClockFace(time);
+    this.title.insertAdjacentHTML("afterbegin", this.newStringMarkUp());
+      const endTime = this.targetDate;
+
+      const currentTime = Date.now();
+      const deltaTime = endTime - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      this.updateClockFace(time);
+    // const time = this.getTimeComponents(0);
+    // this.updateClockFace(time);
   }
 
   start() {
